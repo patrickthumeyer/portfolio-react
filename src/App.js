b8bcad5b1b297a954bde7ed1d03cb9f22a23ab82
patrickthumeyer <p.thumeyer@gmail.com>
2020-08-14 13:37:36 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.scss";
+import ParticlesBackground from "./components/particles/particles";
+// import Hero from "../src/components/hero/hero";
+import "normalize.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="main-screen">
+          <h1 className="headline">Hi there! I´m Patrick.</h1>
+          {/* <Hero /> */}
+          <ParticlesBackground className="particles" />
+        </div>
+        <div className="transition"></div>
+      </div>
     </div>
   );
 }
