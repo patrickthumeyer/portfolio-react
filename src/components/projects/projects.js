d85ddb0844f2c8861787a878e5362abca5f166a9
patrickthumeyer <p.thumeyer@gmail.com>
2020-08-25 13:23:51 +0200

    adjust footer and pflanzy description

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./projects.scss";
 import { Github, WWW } from "../icons/socialIcons";
+import { motion } from "framer-motion";
+
+const projectsContainer = {
+  hidden: {
+    opacity: 1,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.5,
+      when: "beforeChildren",
+    },
+  },
+};
+
+const project = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 function Projects() {
   return (
-    <div className="container">
-      <div class="content">
+    <motion.div
+      className="container"
+      variants={projectsContainer}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div
+        class="content"
+        variants={project}
+        transition={{ delay: 0.1 }}
+      >
         <div class="content-overlay"></div>
         <img
           class="content-image"
@@ -14,6 +47,13 @@ function Projects() {
         />
         <div class="content-details fadeIn-bottom">
           <h3 class="content-title">Pflanzy</h3>
+          <h5 class="subheaderPflanzy">
+            Final Project of DCI WebDev Course - React Native
+          </h5>
+          <p>
+            My role: Expandable container on PlantPage, Explore Section, styling
+            and built of multiple other components
+          </p>
           <div className="linkContainer">
             <a
               href="https://github.com/Pflanzy/Pflanzy-mobile-app"
@@ -25,8 +65,12 @@ function Projects() {
             </a>
           </div>
         </div>
-      </div>
-      <div class="content">
+      </motion.div>
+      <motion.div
+        class="content"
+        variants={project}
+        transition={{ delay: 0.2 }}
+      >
         <div class="content-overlay"></div>
         <img
           class="content-image"
@@ -54,8 +98,12 @@ function Projects() {
             </a>
           </div>
         </div>
-      </div>
-      <div class="content">
+      </motion.div>
+      <motion.div
+        class="content"
+        variants={project}
+        transition={{ delay: 0.3 }}
+      >
         <div class="content-overlay"></div>
         <img
           class="content-image"
@@ -83,8 +131,12 @@ function Projects() {
             </a>
           </div>
         </div>
-      </div>
-      <div class="content">
+      </motion.div>
+      <motion.div
+        class="content"
+        variants={project}
+        transition={{ delay: 0.4 }}
+      >
         <div class="content-overlay"></div>
         <img
           class="content-image"
@@ -112,8 +164,8 @@ function Projects() {
             </a>
           </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
     // <ul className="projectsContainer">
     //   <li className="container">
     //     <img
